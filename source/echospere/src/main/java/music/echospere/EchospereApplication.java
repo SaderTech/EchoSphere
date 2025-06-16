6dc8af8a33876d9f99827085c91beffbeac26f50
Nguyen Quang Hao <haonqhe181952@fpt.edu.vn>
2025-06-17 02:01:01 +0700

    create project in intelliji

---
+++ source/echospere/src/main/java/music/echospere/EchospereApplication.java
@@ -0,0 +1,13 @@
+package music.echospere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchospereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchospereApplication.class, args);
+    }
+
+}
---
+++ source/echospere/src/test/java/music/echospere/EchospereApplicationTests.java
@@ -0,0 +1,13 @@
+package music.echospere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchospereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
